a331ad60ce45e59626eb7e78e540eb76aa9d824c
facundobettella <facundobettella@outlook.com>
2023-02-15 15:26:04 -0300

    First commit: NES Components

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,54 @@
+import "./myLabel.css";
+
+type Size = "normal" | "h1" | "h2" | "h3";
+type Mode = "primary" | "secondary" | "terciary";
+
+interface MyLabelProps {
+  /**
+   * It is the Label content
+   */
+  label: string;
+
+  /**
+   * Label size: it could be  "normal", "h1", "h2", "h3"
+   */
+  size: Size;
+
+  /**
+   * Mode: Is this the principal title on the page? Use primary
+   */
+  mode: Mode;
+  /**
+   * You need caps? use it
+   */
+  allCaps?: boolean;
+
+  fontColor?: string;
+  backgroundColor?: string;
+  borderColor?: string;
+}
+
+export const MyLabel = ({
+  label = "No label",
+  size = "normal",
+  mode = "primary",
+  allCaps = false,
+  fontColor,
+  backgroundColor,
+}: MyLabelProps) => {
+
+  return (
+    <div>
+      <label
+        style={{
+          color: fontColor,
+          backgroundColor: backgroundColor,
+          boxShadow: `0 5px ${backgroundColor}, 0 -5px ${backgroundColor}, 4px 0 ${backgroundColor}, -4px 0 ${backgroundColor}`,
+        }}
+        className={`label text-${mode} ${size}`}
+      >
+        {allCaps ? label.toUpperCase() : label}
+      </label>
+    </div>
+  );
+};
---
+++ src/stories/components/MyLabel.stories.tsx
@@ -0,0 +1,32 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { MyLabel } from "../../components/MyLabel";
+
+export default {
+  title: "NES UI/Etiquetas/MyLabel",
+  component: MyLabel,
+} as ComponentMeta<typeof MyLabel>;
+
+const Template: ComponentStory<typeof MyLabel> = (args) => (
+  <MyLabel {...args} />
+);
+
+export const Primary = Template.bind({});
+Primary.args = {
+  label: "My Label",
+  mode: "primary",
+  size: "normal",
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: "Secondary Label",
+  mode: "secondary",
+  size: "normal",
+};
+
+export const AllCaps = Template.bind({});
+AllCaps.args = {
+  label: "Terciary Label",
+  mode: "terciary",
+  size: "normal",
+};
